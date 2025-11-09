cc7352fa71165627eb343efa745f747171ea5f95
Alexander Riedel <alexander.riedel@riedelmedia.de>
2025-11-09 15:37:29 +0100

    feat: replace support tickets component with tickets component and update imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,13 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { ServerStatusComponent } from "./dashboard/server-status/server-status.component";
 import { TrafficComponent } from "./dashboard/traffic/traffic.component";
-import { SupportTicketsComponent } from "./dashboard/tickets/support-tickets.component";
+import { TicketsComponent } from "./dashboard/tickets/tickets.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, SupportTicketsComponent],
+  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, TicketsComponent],
 })
 export class AppComponent {
 
